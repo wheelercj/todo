813292d74ccf3e1545cdc5e4d2da97de0b4d1233
wheelercj <chrisw289@gmail.com>
2022-02-25 20:20:45 -0800

    Create a separate script for creating individual tasks

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # external imports
-import os
 import re
 import requests
 import uuid
@@ -9,6 +8,7 @@ from dotenv import load_dotenv
 
 # internal import
 from project import Project, Projects
+from create_task import post_task, get_api_token, fetch_projects, get_project_id
 
 
 def main():
@@ -33,46 +33,11 @@ def main():
             add_tasks(project)
 
 
-def get_api_token() -> str:
-    """Gets the API token from a .env file or directly from the user
-
-    If a .env file is not found, the user is asked for the token. If the user
-    provides the token, a .env file with the token is created.
-    """
-    try:
-        api_token = os.environ["my_todoist_api_token"]
-    except KeyError:
-        print("Find your Todoist API token in settings > integrations")
-        api_token = input("and enter it here: ")
-        with open(".env", "a") as file:
-            file.write(f"my_todoist_api_token={api_token}")
-    return api_token
-
-
-def fetch_projects(api_token: str) -> Projects:
-    """Fetches all projects"""
-    return requests.get(
-        "https://api.todoist.com/rest/v1/projects",
-        headers={"Authorization": f"Bearer {api_token}"},
-    ).json()
-
-
 def print_project_names(projects: Projects) -> None:
     """Prints the names of Todoist projects"""
     print("Here are all of your current Todoist projects:")
     for project in projects:
         print(project["name"])
-
-
-def get_project_id(chosen_project_name: str, projects: Projects) -> int:
-    """Gets a Todoist project's ID by its name
-
-    Raises ValueError if the project does not exist.
-    """
-    for project in projects:
-        if project["name"] == chosen_project_name:
-            return project["id"]
-    raise ValueError("Project not found")
 
 
 def add_tasks(project: Project) -> None:
@@ -140,33 +105,6 @@ def post_section(section_title: str, project: Project) -> int:
     ).json()
 
     return response["id"]
-
-
-def post_task(
-    task_title: str,
-    project: Project,
-    section_id: Optional[int],
-    due_date: Optional[str],
-) -> None:
-    """Makes an API request to add a new task to Todoist
-
-    Due dates must be in the YYYY-MM-DD format.
-    """
-    data = {"content": task_title, "due_lang": "en", "project_id": project.id}
-    if section_id:
-        data["section_id"] = section_id
-    if due_date:
-        data["due_date"] = due_date
-
-    requests.post(
-        "https://api.todoist.com/rest/v1/tasks",
-        data=json.dumps(data),
-        headers={
-            "Content-Type": "application/json",
-            "X-Request-Id": str(uuid.uuid4()),
-            "Authorization": f"Bearer {project.api_token}",
-        },
-    ).json()
 
 
 if __name__ == "__main__":
